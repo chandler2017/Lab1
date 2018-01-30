028a8d40dd21030e372d8870f664a6922ace084d
Chandler Mathews <chandlermathews@Chandlers-MacBook-Pro.local>
2018-01-29 18:46:08 -0800

    Lab1

---
+++ NewMath.java
@@ -0,0 +1,31 @@
+/**
+*@author Chandler Mathews
+*@version 1.0
+*.java
+*/
+
+public class NewMath {
+		public static void main(String[] args) {  
+		short s = 2713;
+		long l = 99999999;
+		double d = 4.9123123192031231231232342489;
+
+		System.out.println("s = " + s);
+		System.out.println("l = " + l);
+		System.out.println("d = " + d);
+		System.out.println("s - l = " + (s-l));
+		System.out.println("l - s = " + (l-s));
+		System.out.println("s - d = " + (s-d));
+		System.out.println("d - s = " + (d-s));
+		System.out.println("l - d = " + (l-d));
+		System.out.println("d - l = " + (d-l));
+		System.out.println("s * l = " + (s*l));
+		System.out.println("l * s = " + (l*s));
+		System.out.println("s * d = " + (s*d));
+		System.out.println("d * s = " + (d*s));
+		System.out.println("l * d = " + (l*d));
+		System.out.println("d * l = " + (d*l));
+		
+
+	}
+}
